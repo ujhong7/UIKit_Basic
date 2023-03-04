4c505182b94b9824e3fceb305be1708859cf0ad6
ujhong7 <ujhong7@gmail.com>
2023-03-04 23:43:56 +0900

    no message

---
+++ VC-Cycle/VC-Cycle/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  VC-Cycle
+//
+//  Created by yujaehong on 2023/03/04.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
---
+++ VC-Cycle/VC-Cycle/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  VC-Cycle
+//
+//  Created by yujaehong on 2023/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+        
+    }
+
+
+}
+
