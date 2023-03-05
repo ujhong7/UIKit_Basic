df16bcbe42a54568a86c4cb99b1a3dbd5e97b957
ujhong7 <ujhong7@gmail.com>
2023-03-05 17:21:50 +0900

    no message

--- VC-Cycle/VC-Cycle/SecondViewController.swift
+++ VC-Cycle/VC-Cycle/SecondViewController.swift
@@ -11,9 +11,50 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        print("===== VC-2 viewDidLoad 호출")
     }
     
+    // 뷰가 나타나기 전
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("===== VC-2 viewWillAppear 호출")
+    }
+    
+    // 뷰가 나타난 후
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("===== VC-2 viewDidAppear 호출")
+    }
+    
+    // 뷰가 사라지기 전
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("===== VC-2 viewWillDisappear 호출")
+    }
+    
+    // 뷰가 사라진 후
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("===== VC-2 viewDidDisappear 호출")
+    }
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    
+    deinit{
+        print("===== VC-2 메모리에서 내려감")
+    }
 
 }
+
+// 결과
+// VC-1 viewDidLoad 호출
+// VC-1 viewWillAppear 호출
+// VC-1 viewDidAppear 호출
+// ===== VC-2 viewDidLoad 호출
+// VC-1 viewWillDisappear 호출
+// ===== VC-2 viewWillAppear 호출
+// ===== VC-2 viewDidAppear 호출
+// VC-1 viewDidDisappear 호출
--- VC-Cycle/VC-Cycle/ViewController.swift
+++ VC-Cycle/VC-Cycle/ViewController.swift
@@ -9,13 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    // view(메인뷰)를 만들어 내는 시점/지점 (한번만 호출): 코드로 짤때 뷰 교체 가능 시점
+//    // 스토리보드에선 호출하면 화면 로드 안됨 ⭐️
+//    override func loadView() {
+//        // super.loadView() // 필요X 하면안됨
+//        print("VC-1 loadView 호출")
+//    }
+    
+    // 스토리보드 뷰와 연결된 후 (한번만 호출): 코드와 스토리보드의 뷰가 연결된 후
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        
-        
+        print("VC-1 viewDidLoad 호출")
+    }
+    
+    // 뷰가 나타나기 전
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("VC-1 viewWillAppear 호출")
+    }
+    
+    // 뷰가 나타난 후
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("VC-1 viewDidAppear 호출")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("VC-1 viewWillDisappear 호출")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("VC-1 viewDidDisappear 호출")
     }
 
-
+    deinit{
+        print("VC-1 메모리에서 내려감")
+    }
+    
 }
 
