ff837bf98b1e6586ba376d44bc003f8d3a4753ec
ujhong7 <ujhong7@gmail.com>
2023-04-03 23:57:30 +0900

    no message

--- MusicAPP/MusicAPP/Views/MusicCell.swift
+++ MusicAPP/MusicAPP/Views/MusicCell.swift
@@ -45,25 +45,33 @@ class MusicCell: UITableViewCell {
 
 
     
+    //
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
     
-    // URL -> 이미지를 셋팅하는 메서드
+    
+    //⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+    // URL ===> 이미지를 셋팅하는 메서드
     private func loadImage() {
-//        guard let urlString = self.imageUrl, let url = URL(string: urlString) else { return }
-//        
-//        DispatchQueue.global().async {
+        guard let urlString = self.imageUrl, let url = URL(string: urlString)  else { return }
+        
+        DispatchQueue.global().async {
+        
+            guard let data = try? Data(contentsOf: url) else { return }
+            // 오래걸리는 작업이 일어나고 있는 동안에 url이 바뀔 가능성 제거 ⭐️⭐️⭐️
+            guard urlString == url.absoluteString else { return }
             
-            
-            
-            
+            DispatchQueue.main.async {
+                self.mainImageView.image = UIImage(data: data)
+            }
         }
     }
     
     
     
+    }
     
-}
+    
---
+++ MusicAPP/MusicAPP/Views/MusicCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MusicCollectionViewCell.swift
+//  MusicAPP
+//
+//  Created by yujaehong on 2023/04/03.
+//
+
+import UIKit
+
+
+
+class MusicCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
