770ac34924afcf893a7cbe267a3b9ff06349e268
ujhong7 <ujhong7@gmail.com>
2023-04-13 21:13:41 +0900

    Instagram

---
+++ MyToDoApp/ToDoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoData+CoreDataClass.swift
+//  MyToDoApp
+//
+//  Created by yujaehong on 2023/04/10.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoData)
+public class ToDoData: NSManagedObject {
+
+}
