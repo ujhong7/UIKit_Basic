f0808c70fd11f8d83448fe437d6fd22620d2e303
ujhong7 <ujhong7@gmail.com>
2023-08-05 23:09:50 +0900

    -  Models, Networking, views

---
+++ CoreDataMusicApp/CoreDataMusicApp/Models/MusicSaved+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MusicSaved+CoreDataClass.swift
+//  CoreDataMusicApp
+//
+//  Created by yujaehong on 2023/08/04.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MusicSaved)
+public class MusicSaved: NSManagedObject {
+
+}
---
+++ CoreDataMusicApp/CoreDataMusicApp/Views/SavedMusicCell.swift
@@ -0,0 +1,23 @@
+//
+//  SavedMusicCell.swift
+//  CoreDataMusicApp
+//
+//  Created by yujaehong on 2023/08/04.
+//
+
+import UIKit
+
+class SavedMusicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
